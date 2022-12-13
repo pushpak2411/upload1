549be91fd1edc1ab309c1f248411d487e8a483cb
pushpak2411 <pushpak.igtech@gmail.com>
2022-12-13 14:34:23 +0530

    index file changed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express  from 'express';
+ import express  from 'express';
 const app = express();
 const port = 8000;
 
 app.use('/', (req, res)=>{
-    res.json({msg:`Hello From Server ${port} oops`});
+    res.json({msg:`Hello From Server ${port}`});
 })
 
 app.listen(port, () => {
